730bffd552434f2243ad2f7f1d64ab03b301f409
janeandjack <lcc751993173@gmail.com>
2020-10-23 20:34:10 -0400

    class comp -2

---
+++ src/excerise/class com/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+// function App(){
+//     const appp = todosData.map(toood =>
+//         <TodoItem key={toood.id} ee={toood}  />)
+        
+//     return(
+//         <div className="todo-list">
+//            {appp}
+
+//         </div>
+//     )
+// }
+
+class App extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Code goes here</h1>
+            </div>
+        )
+    }
+}
+
+export default App
